61812532579fe6dcf8c4e1d1e64fe8d050c4030f
mineiro <mineiro@puppetlabs.vm>
2017-11-30 11:10:54 -0800

    incluindo template

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,54 +1,73 @@
 class nginx {
+
+  case $facts['os']['family'] {
+    'RedHat': {
+      $package_name = 'nginx'
+      $docroot      = '/var/www'
+      $confdir      = '/etc/nginx'
+      $blockdir     = '/etc/nginx/conf.d'
+      $logdir       = '/var/log/nginx'
+      $service_user = 'nginx'
+      $service_name = 'nginx'
+    }
+    'Debian': {
+      $package_name = 'nginx'
+      $docroot      = '/var/www'
+      $confdir      = '/etc/nginx'
+      $blockdir     = '/etc/nginx/conf.d'
+      $logdir       = '/var/log/nginx'
+      $service_user = 'www-data'
+      $service_name = 'nginx'
+    }
+    'windows': {
+      $package_name = 'nginx-service'
+      $docroot      = 'C:/ProgramData/nginx/html'
+      $confdir      = 'C:/ProgramData/nginx/conf'
+      $blockdir      = 'C:/ProgramData/nginx/conf.d'
+      $logdir       = 'C:/ProgramData/nginx/logs'
+      $service_user = 'nobody'
+      $service_name = 'nginx'
+    }
+    default: {
+        fail("O sistema ${facts['os']['family']} não é suportado.")
+    }
+  }
+
   File {
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664'
   }
 
-case $facts['os']['family'] {
-  'RedHat': {
-  $package_name = 'nginx'
-  $document_root = '/var/www'
-  $config_directory = '/etc/nginx/'
-  $user_service = 'nginx'
-  }
-  'Debian': {
-  $package_name = 'nginx'
-  $document_root = '/var/www'
-  $config_directory = '/etc/nginx/'
-  $service_name = 'nginx'
-  }
-  'windows': {
-  $package_name = 'nginx-service'
-  $document_root = 'C:/ProgramData/nginx/html'
-  $config_directory = 'C:/ProgramData/nginx/conf'
-  $service_name = 'nginx'
-  }
-  default: {
-    fail("Operating system family ${facts['os']['family']} is not supported.")
-  }
-}
-
-package { $package_name:
-  ensure => 'present',
+  package { $package_name:
+    ensure => installed,
   }
 
-file { $document_root :
-  ensure => 'directory',
+  file { $docroot:
+    ensure => directory,
   }
 
-file { "${document_root}/index.html":
-  ensure     => file,
-  source => 'puppet:///modules/nginx/index.html',
-}
+  file { "${docroot}/index.html":
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html'
+  }
 
-file { "${config_directory}/nginx.conf":
-   ensure => file,
-   source => "puppet:///modules/nginx/${facts['os']['family']}.conf",
-   notify => Service[$service_name],
-                    }
-service { $service_name:
+  file { "${confdir}/nginx.conf":
+    ensure => file,
+    content => epp('nginx/nginx.conf.epp', {
+      nginx_user     => $service_user,
+      nginx_logdir   => $logdir,
+      nginx_confdir  => $confdir,
+      nginx_blockdir => $blockdir,
+      nginx_docroot  => $docroot,
+      }),
+    notify => Service[$service_name],
+  }
+
+  service { $service_name:
     ensure => running,
     enable => true,
+  }
+
 }
-}
+
