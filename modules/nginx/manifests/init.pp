23e173f6844063ddd0f2bd03d08d3b398b879bfe
mineiro <mineiro@puppetlabs.vm>
2017-11-30 10:23:20 -0800

    Ajustado codigo para debian e windows

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,36 +1,54 @@
 class nginx {
   File {
-  owner => 'root',
-  group => 'root',
-  mode  => '0644',
-  }
-
-package { 'nginx':
-  ensure => 'installed',
-  }
-
-file { '/var/www':
-  ensure => 'directory',
-  mode   => '0755',
-}
-
-file { '/var/www/index.html':
-  ensure => 'file',
-  mode   => '0644',
-  source => 'puppet:///modules/nginx/index.html',
-  }
-
-service { 'nginx.service':
-  ensure    => 'running',
-  enable    => 'true',
-  subscribe => File['/etc/nginx/nginx.conf'],
-  }
-
-file { '/etc/nginx/nginx.conf':
-  ensure => 'file',
   owner  => 'root',
   group  => 'root',
   mode   => '0644',
-  source => 'puppet:///modules/nginx/nginx.conf',
   }
+
+case $facts['os']['family'] {
+  'RedHat': {
+  $package_name = 'nginx'
+  $document_root = '/var/www'
+  $config_directory = '/etc/nginx/'
+  $user_service = 'nginx'
+  }
+  'Debian': {
+  $package_name = 'nginx'
+  $document_root = '/var/www'
+  $config_directory = '/etc/nginx/'
+  $service_name = 'nginx'
+  }
+  'windows': {
+  $package_name = 'nginx-service'
+  $document_root = 'C:/ProgramData/nginx/html'
+  $config_directory = 'C:/ProgramData/nginx/conf'
+  $service_name = 'nginx'
+  }
+  default: {
+    fail("Operating system family ${facts['os']['family']} is not supported.")
+  }
+}
+
+package { $package_name:
+  ensure => 'present',
+  }
+
+file { $document_root :
+  ensure => 'directory',
+  }
+
+file { "${document_root}/index.html":
+  ensure     => file,
+  source => 'puppet:///modules/nginx/index.html',
+}
+
+file { "${config_directory}/nginx.conf":
+   ensure => file,
+   source => "puppet:///modules/nginx/${facts['os']['family']}.conf",
+   notify => Service[$service_name],
+                    }
+service { $service_name:
+    ensure => running,
+    enable => true,
+}
 }
